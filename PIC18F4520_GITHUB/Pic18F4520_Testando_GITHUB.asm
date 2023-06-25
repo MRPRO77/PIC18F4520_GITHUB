6120c8eac53a7a6fdc52e1c15bb40e709181f77a
MRPRO77 <mrpro77@hotmail.com>
2023-06-25 10:17:15 -0300

    Modificação no Cabeçalho

--- PIC18F4520_GITHUB/Pic18F4520_Testando_GITHUB.asm
+++ PIC18F4520_GITHUB/Pic18F4520_Testando_GITHUB.asm
@@ -1,8 +1,8 @@
 
 _main:
 
-;Pic18F4520_Testando_GITHUB.c,1 :: 		void main() {
-;Pic18F4520_Testando_GITHUB.c,3 :: 		}
+;Pic18F4520_Testando_GITHUB.c,46 :: 		void main() {
+;Pic18F4520_Testando_GITHUB.c,48 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
--- PIC18F4520_GITHUB/Pic18F4520_Testando_GITHUB.cp
+++ PIC18F4520_GITHUB/Pic18F4520_Testando_GITHUB.cp
@@ -1,4 +1,5 @@
 #line 1 "E:/Curso_GIT_GITHUB_/PIC18F4520_GITHUB/Pic18F4520_Testando_GITHUB.c"
+#line 46 "E:/Curso_GIT_GITHUB_/PIC18F4520_GITHUB/Pic18F4520_Testando_GITHUB.c"
 void main() {
 
 }
